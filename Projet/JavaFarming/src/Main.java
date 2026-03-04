031885e7cd5285c27cb635d4877e8831a0e47684
Achille Coves <achille.coves13@gmail.com>
2026-03-04 15:46:02 +0100

    Upload beginning of the project

---
+++ Projet/JavaFarming/src/Main.java
@@ -0,0 +1,28 @@
+import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception  {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/menu.fxml"));
+        primaryStage.setTitle("Farming");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
